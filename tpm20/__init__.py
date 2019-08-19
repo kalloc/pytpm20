c12370c83758c8855e1401edf1cf7ce7fc374e22
Nikita Kuznetsov <me@daedalus.ru>
2019-08-19 09:18:49 +0200

    fix: remove useless typing, sort imports

--- tpm20/__init__.py
+++ tpm20/__init__.py
@@ -1,24 +1,22 @@
-import typing
+from hashlib import sha256
 
 import _tpm20
-
 from ecdsa import VerifyingKey
 from ecdsa.util import sigdecode_der
-from hashlib import sha256
 
 
 class TPM20:
     def __init__(self):
         _tpm20.setup()
 
-    def sign(self, data: bytes) -> typing.Union[Exception, bytes]:
+    def sign(self, data: bytes) -> bytes:
         return _tpm20.sign(data)
 
-    def random(self, size: int) -> typing.Union[Exception, bytes]:
+    def random(self, size: int) -> bytes:
         return _tpm20.random(size)
 
     @property
-    def public_key(self) -> typing.Union[Exception, bytes]:
+    def public_key(self) -> bytes:
         return _tpm20.public()
 
     def verify(self, signature: bytes, message: bytes) -> bool:
@@ -31,4 +29,3 @@ del TPM20
 
 __all__ = ('tpm20')
 __version__ = '0.1.0'
-
